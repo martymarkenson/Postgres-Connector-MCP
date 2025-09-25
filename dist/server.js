34bfa0e8ffc0806304835c9fe9d8eb04f95880cb
martymarkenson <5985475+martymarkenson@users.noreply.github.com>
2025-09-25 18:33:39 -0400

    npm build

--- dist/server.js
+++ dist/server.js
@@ -19,6 +19,7 @@ const getDb = () => {
         database: process.env.DB_NAME,
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
+        ssl: 'prefer'
     });
     return db;
 };
