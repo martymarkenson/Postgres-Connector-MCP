d380efee0231ee908461cd762b49fac0199c4299
martymarkenson <5985475+martymarkenson@users.noreply.github.com>
2025-09-25 18:33:29 -0400

    ssl prefer

--- src/server.ts
+++ src/server.ts
@@ -24,6 +24,8 @@ const getDb = () => {
         database: process.env.DB_NAME,
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
+        ssl: 'prefer'
+
       });
   return db;
 };
